5eaa8c41c1eb821227a55677150c69a22d15e555
Larissa Cerqueira <larissa.cerqueira.ribeiro@gmail.com>
2023-06-19 14:49:38 -0300

    manu

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/configuration/icon/ExportDDMFormInstancePortletConfigurationIcon.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/configuration/icon/ExportDDMFormInstancePortletConfigurationIcon.java
@@ -68,7 +68,7 @@ public class ExportDDMFormInstancePortletConfigurationIcon
 
 	@Override
 	public String getMessage(PortletRequest portletRequest) {
-		return _language.get(getLocale(portletRequest), "export");
+		return _language.get(getLocale(portletRequest), "manu");
 	}
 
 	@Override
