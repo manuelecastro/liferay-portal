4cd8a783b812a47534536fedcf64f04a03d8640b
Manuele Castro <manuele.castro@liferay.com>
2025-09-11 18:41:43 -0300

    LPD-61657 Add resource permission action for attachment ObjectFields. Use a prefix for the actionId so it won't conflict with standalone ObjectAction actionId, in case the ObjectField and the ObjectAction have same name.

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldLocalServiceImpl.java
@@ -13,6 +13,7 @@ import com.liferay.object.constants.ObjectFieldConstants;
 import com.liferay.object.constants.ObjectFieldSettingConstants;
 import com.liferay.object.constants.ObjectRelationshipConstants;
 import com.liferay.object.constants.ObjectValidationRuleSettingConstants;
+import com.liferay.object.definition.security.permission.resource.util.ObjectDefinitionResourcePermissionUtil;
 import com.liferay.object.definition.util.ObjectDefinitionUtil;
 import com.liferay.object.exception.DuplicateObjectFieldExternalReferenceCodeException;
 import com.liferay.object.exception.ObjectDefinitionEnableLocalizationException;
@@ -83,6 +84,8 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.module.service.Snapshot;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.security.permission.ResourceActions;
+import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.Base64;
@@ -910,6 +913,21 @@ public class ObjectFieldLocalServiceImpl
 			objectField, objectDefinition, objectFieldBusinessType,
 			objectFieldSettings, null);
 
+		if (objectDefinition.isApproved() &&
+			Objects.equals(
+				objectField.getBusinessType(),
+				ObjectFieldConstants.BUSINESS_TYPE_ATTACHMENT)) {
+
+			try {
+				ObjectDefinitionResourcePermissionUtil.populateResourceActions(
+					null, null, objectDefinition, objectFieldLocalService,
+					_portletLocalService, _resourceActions, null);
+			}
+			catch (Exception exception) {
+				ReflectionUtil.throwException(exception);
+			}
+		}
+
 		if (!objectDefinition.isApproved() ||
 			ObjectFieldUtil.isMetadata(name) ||
 			(system && objectDefinition.isUnmodifiableSystemObject())) {
@@ -1146,6 +1164,11 @@ public class ObjectFieldLocalServiceImpl
 		if (Objects.equals(
 				objectField.getBusinessType(),
 				ObjectFieldConstants.BUSINESS_TYPE_ATTACHMENT)) {
+
+			if (objectDefinition.isApproved()) {
+				_resourceActions.removeModelResource(
+					objectDefinition.getClassName(), objectField.getName());
+			}
 
 			ObjectFieldSetting objectFieldSetting =
 				_objectFieldSettingPersistence.fetchByOFI_N(
@@ -1981,6 +2004,9 @@ public class ObjectFieldLocalServiceImpl
 	@Reference
 	private ObjectViewLocalService _objectViewLocalService;
 
+	@Reference
+	private PortletLocalService _portletLocalService;
+
 	private final Set<String> _readOnlyObjectFieldNames = SetUtil.fromArray(
 		"createDate", "creator", "id", "modifiedDate", "status");
 	private final Set<String> _reservedNames = SetUtil.fromArray(
@@ -1990,6 +2016,9 @@ public class ObjectFieldLocalServiceImpl
 		"modifieddate", "reviewdate", "status", "statusbyuserid",
 		"statusbyusername", "statusdate", "taxonomycategoryids", "userid",
 		"username");
+
+	@Reference
+	private ResourceActions _resourceActions;
 
 	@Reference
 	private SystemObjectDefinitionManagerRegistry
