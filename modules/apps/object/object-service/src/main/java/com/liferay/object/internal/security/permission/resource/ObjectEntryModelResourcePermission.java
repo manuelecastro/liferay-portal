7592571eba02761820d7cd48220c48fa7652bdad
Manuele Castro <manuele.castro@liferay.com>
2025-09-22 15:09:09 -0300

    LPD-61657 Verify attachment field permissions using the references of the original ObjectEntry and ObjectDefinition

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/security/permission/resource/ObjectEntryModelResourcePermission.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/security/permission/resource/ObjectEntryModelResourcePermission.java
@@ -141,6 +141,8 @@ public class ObjectEntryModelResourcePermission
 
 		if ((objectEntry.getRootObjectEntryId() != 0) &&
 			!_isObjectActionName(
+				actionId, objectEntry.getObjectDefinitionId()) &&
+			!_isObjectFieldName(
 				actionId, objectEntry.getObjectDefinitionId())) {
 
 			ObjectEntry rootObjectEntry =
@@ -378,6 +380,27 @@ public class ObjectEntryModelResourcePermission
 					ObjectActionTriggerConstants.KEY_STANDALONE)) {
 
 			if (Objects.equals(objectAction.getName(), actionId)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private boolean _isObjectFieldName(
+		String actionId, long objectDefinitionId) {
+
+		for (ObjectField objectField :
+				_objectFieldLocalService.getObjectFieldsByBusinessType(
+					objectDefinitionId,
+					ObjectFieldConstants.BUSINESS_TYPE_ATTACHMENT)) {
+
+			if (Objects.equals(
+					ObjectFieldConstants.
+						ATTACHMENT_FIELD_DOWNLOAD_ACTION_ID_PREFIX +
+							objectField.getName(),
+					actionId)) {
+
 				return true;
 			}
 		}
