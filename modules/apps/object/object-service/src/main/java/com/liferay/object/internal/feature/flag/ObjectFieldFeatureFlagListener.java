4de0d0f0deee2d630af3be97ae22c27b362d76c0
Manuele Castro <manuele.castro@liferay.com>
2025-09-23 16:25:22 -0300

    LPD-61657 Add FF listener so that attachment ObjectField ActionIds are added when enabled and removed when disabled.

---
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/feature/flag/ObjectFieldFeatureFlagListener.java
@@ -0,0 +1,183 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.internal.feature.flag;
+
+import com.liferay.object.constants.ObjectFieldConstants;
+import com.liferay.object.definition.security.permission.resource.util.ObjectDefinitionResourcePermissionUtil;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagListener;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.ResourceAction;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.ResourcePermission;
+import com.liferay.portal.kernel.security.permission.ResourceActions;
+import com.liferay.portal.kernel.service.PortletLocalService;
+import com.liferay.portal.kernel.service.ResourceActionLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.language.override.service.PLOEntryLocalService;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Manuele Castro
+ */
+@Component(
+	property = "feature.flag.key=LPD-17564", service = FeatureFlagListener.class
+)
+public class ObjectFieldFeatureFlagListener implements FeatureFlagListener {
+
+	@Override
+	public void onValue(
+		long companyId, String featureFlagKey, boolean enabled) {
+
+		if (Objects.equals(featureFlagKey, "LPD-17564")) {
+			List<ObjectDefinition> objectDefinitions =
+				_objectDefinitionLocalService.getObjectDefinitions(
+					companyId, WorkflowConstants.STATUS_APPROVED);
+
+			for (ObjectDefinition objectDefinition : objectDefinitions) {
+				List<ObjectField> objectFields =
+					_objectFieldLocalService.getObjectFieldsByBusinessType(
+						objectDefinition.getObjectDefinitionId(),
+						ObjectFieldConstants.BUSINESS_TYPE_ATTACHMENT);
+
+				for (ObjectField objectField : objectFields) {
+					String actionId =
+						ObjectFieldConstants.
+							ATTACHMENT_FIELD_DOWNLOAD_ACTION_ID_PREFIX +
+								objectField.getName();
+
+					if (enabled) {
+						ResourceAction resourceAction =
+							_resourceActionLocalService.fetchResourceAction(
+								objectDefinition.getClassName(), actionId);
+
+						if (resourceAction != null) {
+							continue;
+						}
+
+						try {
+							ObjectDefinitionResourcePermissionUtil.
+								populateResourceActions(
+									Collections.singletonList(objectField),
+									_language, null, objectDefinition,
+									_objectFieldLocalService,
+									_ploEntryLocalService, _portletLocalService,
+									_resourceActions, null);
+
+							_addOrUpdateObjectFieldResourceActionPLOEntries(
+								objectField);
+
+							_updateResourcePermissions(
+								objectDefinition, objectField);
+						}
+						catch (Exception exception) {
+							_log.error(exception);
+						}
+					}
+					else {
+						_resourceActions.removeModelResource(
+							objectDefinition.getClassName(), actionId);
+
+						_ploEntryLocalService.deletePLOEntries(
+							objectField.getCompanyId(), "action." + actionId);
+					}
+				}
+			}
+		}
+	}
+
+	private void _addOrUpdateObjectFieldResourceActionPLOEntries(
+			ObjectField objectField)
+		throws PortalException {
+
+		for (Locale locale : _language.getAvailableLocales()) {
+			String languageId = LocaleUtil.toLanguageId(locale);
+
+			_ploEntryLocalService.addOrUpdatePLOEntry(
+				objectField.getCompanyId(), objectField.getUserId(),
+				StringBundler.concat(
+					"action.",
+					ObjectFieldConstants.
+						ATTACHMENT_FIELD_DOWNLOAD_ACTION_ID_PREFIX,
+					objectField.getName()),
+				languageId,
+				LanguageUtil.format(
+					locale, "download-x", objectField.getLabel(locale)));
+		}
+	}
+
+	private void _updateResourcePermissions(
+			ObjectDefinition objectDefinition, ObjectField objectField)
+		throws PortalException {
+
+		List<ResourcePermission> resourcePermissions =
+			_resourcePermissionLocalService.getResourcePermissions(
+				objectDefinition.getCompanyId(),
+				objectDefinition.getClassName(),
+				ResourceConstants.SCOPE_INDIVIDUAL);
+
+		for (ResourcePermission resourcePermission : resourcePermissions) {
+			String objectFieldActionId =
+				ObjectFieldConstants.
+					ATTACHMENT_FIELD_DOWNLOAD_ACTION_ID_PREFIX +
+						objectField.getName();
+
+			if (!resourcePermission.hasActionId(objectFieldActionId) &&
+				resourcePermission.isViewActionId()) {
+
+				resourcePermission.addResourceAction(objectFieldActionId);
+
+				_resourcePermissionLocalService.updateResourcePermission(
+					resourcePermission);
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ObjectFieldFeatureFlagListener.class);
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference
+	private ObjectFieldLocalService _objectFieldLocalService;
+
+	@Reference
+	private PLOEntryLocalService _ploEntryLocalService;
+
+	@Reference
+	private PortletLocalService _portletLocalService;
+
+	@Reference
+	private ResourceActionLocalService _resourceActionLocalService;
+
+	@Reference
+	private ResourceActions _resourceActions;
+
+	@Reference
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+}
