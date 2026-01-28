98fb71f58c6e83054ca9299d9f34b416397a28ee
Lucas Miranda <lucas.miranda@liferay.com>
2026-01-28 15:31:51 -0300

    LPD-74662 Handle all scope levels

--- modules/apps/product-analytics/product-analytics-web/src/main/java/com/liferay/product/analytics/web/internal/display/context/BaseDisplayContext.java
+++ modules/apps/product-analytics/product-analytics-web/src/main/java/com/liferay/product/analytics/web/internal/display/context/BaseDisplayContext.java
@@ -5,6 +5,7 @@
 
 package com.liferay.product.analytics.web.internal.display.context;
 
+import com.liferay.configuration.admin.constants.ConfigurationAdminPortletKeys;
 import com.liferay.layout.utility.page.kernel.constants.LayoutUtilityPageEntryConstants;
 import com.liferay.layout.utility.page.kernel.provider.LayoutUtilityPageEntryLayoutProvider;
 import com.liferay.petra.string.StringPool;
@@ -16,17 +17,17 @@ import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.model.LayoutSet;
-import com.liferay.portal.kernel.service.LayoutSetLocalServiceUtil;
 import com.liferay.portal.kernel.settings.LocalizedValuesMap;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.product.analytics.web.internal.configuration.ProductAnalyticsConfiguration;
 import com.liferay.product.analytics.web.internal.constants.ProductAnalyticsCookiesConstants;
+
+import jakarta.portlet.PortletRequest;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -165,19 +166,27 @@ public abstract class BaseDisplayContext {
 				(ThemeDisplay)httpServletRequest.getAttribute(
 					WebKeys.THEME_DISPLAY);
 
-			LayoutSet layoutSet = LayoutSetLocalServiceUtil.fetchLayoutSet(
-				themeDisplay.getServerName());
+			String portletId = PortalUtil.getPortletId(
+				(PortletRequest)httpServletRequest.getAttribute(
+					JavaConstants.JAKARTA_PORTLET_REQUEST));
 
-			if (layoutSet != null) {
-				Group group = layoutSet.getGroup();
+			if (portletId.equals(
+					ConfigurationAdminPortletKeys.INSTANCE_SETTINGS)) {
+
+				return ConfigurationProviderUtil.getCompanyConfiguration(
+					ProductAnalyticsConfiguration.class,
+					themeDisplay.getCompanyId());
+			}
+			else if (portletId.equals(
+						ConfigurationAdminPortletKeys.SITE_SETTINGS)) {
 
 				return ConfigurationProviderUtil.getGroupConfiguration(
-					ProductAnalyticsConfiguration.class, group.getGroupId());
+					ProductAnalyticsConfiguration.class,
+					themeDisplay.getScopeGroupId());
 			}
 
-			return ConfigurationProviderUtil.getCompanyConfiguration(
-				ProductAnalyticsConfiguration.class,
-				themeDisplay.getCompanyId());
+			return ConfigurationProviderUtil.getSystemConfiguration(
+				ProductAnalyticsConfiguration.class);
 		}
 		catch (Exception exception) {
 			_log.error(exception);
