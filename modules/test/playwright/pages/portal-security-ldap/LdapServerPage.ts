95867152da7ce72158ed28964c0408b983f3bc9c
Lucas Miranda <lucas.miranda@liferay.com>
2026-02-05 13:12:13 -0300

    LPD-73121 Replace poshi test with playwright test

--- modules/test/playwright/pages/portal-security-ldap/LdapServerPage.ts
+++ modules/test/playwright/pages/portal-security-ldap/LdapServerPage.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {Locator, Page} from '@playwright/test';
+import {Locator, Page, expect} from '@playwright/test';
 
 import {TLdapServer} from '../../helpers/LdapConfigurationHelper';
 import {waitForAlert} from '../../utils/waitForAlert';
@@ -209,6 +209,9 @@ export class LdapServerPage {
 		if (ldapServer.clockSkew) {
 			await this.clockSkew.fill(ldapServer.clockSkew.toString());
 		}
+
+		await expect(this.credentials).toHaveAttribute('type', 'password');
+
 		if (ldapServer.credentials) {
 			await this.credentials.fill(ldapServer.credentials);
 		}
