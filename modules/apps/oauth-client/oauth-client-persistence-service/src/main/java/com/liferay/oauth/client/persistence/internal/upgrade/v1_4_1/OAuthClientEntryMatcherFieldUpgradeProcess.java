ef660787c3f7825099f19140956c1f1543995518
Manuele Castro <manuele.castro@liferay.com>
2026-02-23 16:28:13 -0300

    LPD-67470 Add upgrade process

---
+++ modules/apps/oauth-client/oauth-client-persistence-service/src/main/java/com/liferay/oauth/client/persistence/internal/upgrade/v1_4_1/OAuthClientEntryMatcherFieldUpgradeProcess.java
@@ -0,0 +1,109 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.oauth.client.persistence.internal.upgrade.v1_4_1;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
+import com.liferay.portal.kernel.upgrade.UpgradeStep;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.util.Dictionary;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+/**
+ * @author Manuele Castro
+ */
+public class OAuthClientEntryMatcherFieldUpgradeProcess extends UpgradeProcess {
+
+	public OAuthClientEntryMatcherFieldUpgradeProcess(
+		ConfigurationAdmin configurationAdmin) {
+
+		_configurationAdmin = configurationAdmin;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
+				"select authServerWellKnownURI, clientId, companyId, " +
+					"oAuthClientEntryId from OAuthClientEntry");
+			ResultSet resultSet = preparedStatement1.executeQuery()) {
+
+			while (resultSet.next()) {
+				try (PreparedStatement preparedStatement2 =
+						connection.prepareStatement(
+							"update OAuthClientEntry set matcherField = ? " +
+								"WHERE oAuthClientEntryId = ?")) {
+
+					preparedStatement2.setString(
+						1,
+						_getMatcherField(
+							resultSet.getString("authServerWellKnownURI"),
+							resultSet.getString("clientId"),
+							resultSet.getLong("companyId")));
+					preparedStatement2.setLong(
+						2, resultSet.getLong("oAuthClientEntryId"));
+
+					preparedStatement2.execute();
+				}
+			}
+		}
+	}
+
+	@Override
+	protected UpgradeStep[] getPreUpgradeSteps() {
+		return new UpgradeStep[] {
+			UpgradeProcessFactory.addColumns(
+				"OAuthClientEntry", "matcherField VARCHAR(75) null")
+		};
+	}
+
+	private String _getMatcherField(
+			String authServerWellKnownURI, String clientId, long companyId)
+		throws Exception {
+
+		String filterString = StringBundler.concat(
+			"(&(companyId=", companyId, ")(discoveryEndpoint=",
+			authServerWellKnownURI, ")(openIdConnectClientId=", clientId, "))");
+
+		try (PreparedStatement preparedStatement = connection.prepareStatement(
+				"select issuer from OAuthClientASLocalMetadata where " +
+					"localWellKnownURI = ?")) {
+
+			preparedStatement.setString(1, authServerWellKnownURI);
+
+			ResultSet resultSet = preparedStatement.executeQuery();
+
+			if (resultSet.next()) {
+				filterString = StringBundler.concat(
+					"(&(companyId=", companyId, ")(issuerURL=",
+					resultSet.getString("issuer"), ")(openIdConnectClientId=",
+					clientId, "))");
+			}
+		}
+
+		Configuration[] configurations = _configurationAdmin.listConfigurations(
+			filterString);
+
+		if (ArrayUtil.isEmpty(configurations)) {
+			return "email";
+		}
+
+		Dictionary<String, Object> properties =
+			configurations[0].getProperties();
+
+		return GetterUtil.getString(properties.get("matcherField"));
+	}
+
+	private final ConfigurationAdmin _configurationAdmin;
+
+}
