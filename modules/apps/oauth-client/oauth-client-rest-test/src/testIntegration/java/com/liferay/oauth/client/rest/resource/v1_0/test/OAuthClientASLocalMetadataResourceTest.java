6dc87f0988f6237e58b225a026e4edbb5145e65b
Manuele Castro <manuele.castro@liferay.com>
2026-03-31 14:38:53 -0300

    LPD-74226 Add test implementation

--- modules/apps/oauth-client/oauth-client-rest-test/src/testIntegration/java/com/liferay/oauth/client/rest/resource/v1_0/test/OAuthClientASLocalMetadataResourceTest.java
+++ modules/apps/oauth-client/oauth-client-rest-test/src/testIntegration/java/com/liferay/oauth/client/rest/resource/v1_0/test/OAuthClientASLocalMetadataResourceTest.java
@@ -6,15 +6,104 @@
 package com.liferay.oauth.client.rest.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.oauth.client.rest.client.dto.v1_0.OAuthClientASLocalMetadata;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.FeatureFlag;
+import com.liferay.portal.test.rule.FeatureFlags;
 
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 /**
  * @author Manuele Castro
  */
-@Ignore
+@FeatureFlags(featureFlags = @FeatureFlag(value = "LPD-49855"))
 @RunWith(Arquillian.class)
 public class OAuthClientASLocalMetadataResourceTest
 	extends BaseOAuthClientASLocalMetadataResourceTestCase {
+
+	@Override
+	protected OAuthClientASLocalMetadata randomOAuthClientASLocalMetadata()
+		throws Exception {
+
+		OAuthClientASLocalMetadata oAuthClientASLocalMetadata =
+			super.randomOAuthClientASLocalMetadata();
+
+		String issuer =
+			"https://" + StringUtil.toLowerCase(RandomTestUtil.randomString());
+
+		oAuthClientASLocalMetadata.setIssuer(issuer);
+		oAuthClientASLocalMetadata.setMetadataJSON(
+			JSONUtil.put(
+				"issuer", issuer
+			).put(
+				"subject_types_supported", JSONUtil.put("public")
+			).put(
+				"token_endpoint", issuer + "/token"
+			).toString());
+		oAuthClientASLocalMetadata.setOAuthASMetadataJSON(
+			JSONUtil.put(
+				"registration_endpoint", issuer + "/registration"
+			).toString());
+
+		return oAuthClientASLocalMetadata;
+	}
+
+	@Override
+	protected OAuthClientASLocalMetadata
+			testBatchEngineDeleteImportTask_addOAuthClientASLocalMetadata()
+		throws Exception {
+
+		return oAuthClientASLocalMetadataResource.
+			postOAuthClientASLocalMetadata(randomOAuthClientASLocalMetadata());
+	}
+
+	@Override
+	protected OAuthClientASLocalMetadata
+			testDeleteOAuthClientASLocalMetadataByExternalReferenceCode_addOAuthClientASLocalMetadata()
+		throws Exception {
+
+		return oAuthClientASLocalMetadataResource.
+			postOAuthClientASLocalMetadata(randomOAuthClientASLocalMetadata());
+	}
+
+	@Override
+	protected OAuthClientASLocalMetadata
+			testGetOAuthClientASLocalMetadataByExternalReferenceCode_addOAuthClientASLocalMetadata()
+		throws Exception {
+
+		return oAuthClientASLocalMetadataResource.
+			postOAuthClientASLocalMetadata(randomOAuthClientASLocalMetadata());
+	}
+
+	@Override
+	protected OAuthClientASLocalMetadata
+			testGetOAuthClientASLocalMetadatasPage_addOAuthClientASLocalMetadata(
+				OAuthClientASLocalMetadata oAuthClientASLocalMetadata)
+		throws Exception {
+
+		return oAuthClientASLocalMetadataResource.
+			postOAuthClientASLocalMetadata(oAuthClientASLocalMetadata);
+	}
+
+	@Override
+	protected OAuthClientASLocalMetadata
+			testPostOAuthClientASLocalMetadata_addOAuthClientASLocalMetadata(
+				OAuthClientASLocalMetadata oAuthClientASLocalMetadata)
+		throws Exception {
+
+		return oAuthClientASLocalMetadataResource.
+			postOAuthClientASLocalMetadata(oAuthClientASLocalMetadata);
+	}
+
+	@Override
+	protected OAuthClientASLocalMetadata
+			testPutOAuthClientASLocalMetadataByExternalReferenceCode_addOAuthClientASLocalMetadata()
+		throws Exception {
+
+		return oAuthClientASLocalMetadataResource.
+			postOAuthClientASLocalMetadata(randomOAuthClientASLocalMetadata());
+	}
+
 }
